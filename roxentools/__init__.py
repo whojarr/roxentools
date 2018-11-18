939014c570fdc91d4e5baf5bed0fa8986d45bd38
David Hunter <dhunter@module.net.nz>
2018-11-19 08:02:51 +1300

    add missing modules from main __init__

--- roxentools/__init__.py
+++ roxentools/__init__.py
@@ -1,12 +1,16 @@
+
+from .config import *
+from .database import *
 from .exceptions import *
 from .interface import *
-from .config import *
+from .module import *
+from .rest import *
 from .server import *
-from .database import *
 from .site import *
 
 __all__ = ['interface_call',
            'server_version', 'server_restart',
+           'module_reload',
            'db_create', 'db_delete', 'db_permission',
            'site_exists', 'site_config_copy'
            ]
