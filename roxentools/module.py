d5b4c6e3c6f2fbdc16f2b05150aa135ffc27288b
David Hunter <dhunter@module.net.nz>
2018-11-19 08:41:14 +1300

    return proper response dict from module_reload for api results

--- roxentools/module.py
+++ roxentools/module.py
@@ -18,8 +18,13 @@ def reload(username=None, password=None, host="https://localhost:9999", site="te
     webhaven_rc_handler (inhouse)
      
     """
-    usite = urllib.quote(site.replace("_", " "))
+    usite = quote(site.replace("_", " "))
     url = host + "/rest/configurations/" + usite + "/modules/" + module + "%21" + str(copy) + "/actions/Reload"
+
     response = requests.put(url, auth=(username, password), verify=False, headers={"X-Roxen-API":"1"})
     if not response.text.strip() == "1":
-        raise ValueError(response.json()['error'])
+        return response.json()
+    else:
+        result = {}
+        result['result'] = 'reloaded'
+        return result
