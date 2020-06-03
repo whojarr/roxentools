75d800ed0701a68c30c4f10b0f6c547e1ebf4376
David Hunter <dhunter@module.net.nz>
2020-06-03 23:50:20 +1200

    DO-439 add roxentools.module.installed() and add()

--- roxentools/module.py
+++ roxentools/module.py
@@ -12,9 +12,9 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 def reload(username=None, password=None, host="https://localhost:9999", site="test", module="rxmltags", copy=0):
     """
     tested modules to reload
-    
     rxmltags
     webhaven (inhouse)
+    webhaven_pml
     webhaven_rc_handler (inhouse)
      
     """
@@ -28,3 +28,39 @@ def reload(username=None, password=None, host="https://localhost:9999", site="te
         result = {}
         result['result'] = 'reloaded'
         return result
+
+
+def add(username=None, password=None, host="https://localhost:9999", site="test", module="rxmltags", copy=0):
+    """
+    tested modules to add
+    
+    static_resources
+     
+    """
+    usite = quote(site.replace("_", " "))
+    url = host + "/rest/configurations/" + usite + "/modules/" + module + "/"
+
+    response = requests.post(url, auth=(username, password), verify=False, headers={"X-Roxen-API":"1"})
+    if not response.text.strip() == "1":
+        return json.loads(response.text)
+    else:
+        return None
+
+
+def installed(username=None, password=None, host="https://localhost:9999", site="test", module="rxmltags", copy=0):
+    """
+    tested modules to add
+    
+    static_resources
+     
+    """
+    usite = quote(site.replace("_", " "))
+    url = host + "/rest/configurations/" + usite + "/modules/"
+
+    response = requests.get(url, auth=(username, password), verify=False, headers={"X-Roxen-API":"1", "Content-Type":"application/json"})
+    modules = json.loads(response.text)
+    if "{}!0".format(module) in modules:
+        return True
+
+    return False
+
